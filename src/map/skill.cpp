7ea9d17d0e1b4b0eeaf0150252ad77295430f871
flyingmingming <classicaquastrialro@gmail.com>
2024-11-21 03:41:52 +0800

    modified:   src/map/skill.cpp

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -22701,8 +22701,8 @@ void skill_toggle_magicpower(struct block_list *bl, uint16 skill_id)
 			if(bl->type == BL_PC){// update current display.
 				clif_updatestatus(*((map_session_data *)bl),SP_MATK1);
 				clif_updatestatus(*((map_session_data *)bl),SP_MATK2);
-				//find me
-				//status_change_end(bl, SC_MAGICPOWER);	
+				
+				sc->getSCE(SC_MAGICPOWER)->val4 = 0;
 			}
 		}
 			
